2abcd7703a67f1b3b141d51dda29a79ccc0bdb07
ariful <arifulislam321054@gmail.com>
2023-05-31 22:09:25 +0600

    send message and console.log

--- server.js
+++ server.js
@@ -8,6 +8,37 @@ const port = process.env.PORT || 5000;
 
 connectDB();
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`server is ok and running on port: ${port}`);
+});
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+  });
+
+  socket.on("new message", (newMessageRecieved) => {
+    console.log(newMessageRecieved);
+    let chat = newMessageRecieved.chat;
+    if (!chat.users) {
+      return console.log("chat.users no foundsssssss");
+    }
+
+    chat.users.forEach((user) => {
+      if (user._id === newMessageRecieved.sender._id) return;
+      socket.to(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
 });
