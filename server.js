fb6009754646f5bf43116104395c5a91b4b6c524
ariful <arifulislam321054@gmail.com>
2023-06-01 07:20:32 +0600

    socket function update

--- server.js
+++ server.js
@@ -29,6 +29,9 @@ io.on("connection", (socket) => {
     socket.join(room);
   });
 
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
   socket.on("new message", (newMessageRecieved) => {
     console.log(newMessageRecieved);
     let chat = newMessageRecieved.chat;
